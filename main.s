b5b1039ac755f41cf775ad3eef0ee25c0e0fe035
Nicholas Geiser <nicholas.geiser@maine.edu>
2024-10-21 11:51:30 -0400

    RUN cmd working

--- main.s
+++ main.s
@@ -1,4 +1,3 @@
-;65c51 Mapping registers
 UART_DATA = $8020
 UART_STAT = $8021
 UART_CMD  = $8022
@@ -10,12 +9,11 @@ OIRA = $8001
 DDRB = $8002
 DDRA = $8003
 
-CMD_LOC = $1000
-WRITE_CMD_LOC = $1010
+CMD_LOC = $0
 
 MAX_CMD_SIZE = $A
 
-.org $8040 ; ROM has an offset of 8040
+.org $8040 ; ROM has an offset of 8024
 
 init:
     lda #0
@@ -27,12 +25,51 @@ init:
     lda #%00001011 ; Interupts active low, no parity, no echo
     sta UART_CMD
 
-
+    jmp new_line
 
 main:
-    jsr get_cmd
-    jsr handle_cmd
+    ;receive
+    ldx #0
+read_char:
+    lda UART_STAT
+    and #$08 ;check rx buffer flag
+    beq read_char ;if empty, try again
+    lda UART_DATA
+    sta CMD_LOC,x ;Store to the CMD_LOC
+    inx
+    cmp #13
+    bne send_char
+;Run only if '\r' enterd
+    sta UART_DATA
+    jsr sleep
+    lda #$a ;newline
+    sta UART_DATA
+    jsr sleep
+    jmp handle_cmd
+
+send_char:
+    ;send
+    sta UART_DATA
+    jsr sleep
+    jmp read_char
+
+;print the new line sequence
+new_line:
+    pha
+    ldx #0
+new_line_loop:
+    lda new_cmd_line,x ;Load a with x in string
+    cmp #0 ;If null (end of string)
+    beq new_line_end ;If done with string exit 
+    sta UART_DATA
+    jsr sleep ;Send char over UART_CMD
+    inx
+    jmp new_line_loop
+new_line_end:
+    pla
     jmp main
+
+
 
 sleep:
   pha ; preserve the accumulator
@@ -44,131 +81,28 @@ sleep_loop:
   pla ; pop the accumulator back off
   rts ; return from subroutine
 
-;Loops until a char is sent, returns in A reg
-read_char:
-    pha
-read_char_loop:
-    lda UART_STAT
-    and #$08 ;check rx buffer flag
-    beq read_char_loop ;if empty, try again
-    lda UART_DATA
-    jsr send_char ;Echo back char
-    pla
-    rts
-
-;send char in A reg
-send_char:
-    sta UART_DATA
-    jsr sleep
-    jmp main
-
-;print the new line sequence
-new_line:
-    pha
-    ldx #0
-new_line_loop:
-    lda new_cmd_line,x ;Load a with x in string
-    cmp #0 ;If null (end of string)
-    beq new_line_end ;If done with string exit 
-    jsr send_char ;Send char over UART_CMD
-    inx
-    jmp new_line_loop
-new_line_end:
-    pla
-    rts
-    
-;Gets a whole command
-get_cmd:
-  pha
-  ldx #0
-get_cmd_loop:
-  jsr read_char ;Get a char
-  cmp #13 ;13 = carriage return
-  beq get_cmd_end
-  sta CMD_loc,x ;store to the next location
-  cpx MAX_CMD_SIZE ; Prevent an infinitely long cmd
-  beq get_cmd_end
-  inx
-  beq get_cmd_loop
-
-get_cmd_end:
-  inx
-  lda #0 ;end of string
-  sta CMD_LOC,x ;store to the cmd location
-  lda #$a ;Send newline char
-  jsr send_char
-  pla
-  rts
 
 handle_cmd:
   ldx #0
 run_cmd:
   lda CMD_LOC,x
-  cmp 'r'
-  bne write_cmd
+  cmp #114 ;r
+  bne inv_cmd
   inx
   lda CMD_LOC,x
-  cmp 'u'
-  bne write_cmd
+  cmp #117 ;u
+  bne inv_cmd
   inx
   lda CMD_LOC,x
-  cmp 'n'
-  bne write_cmd
-  jsr exec_run_cmd
+  cmp #110 ;n
+  bne inv_cmd
+  jmp exec_run_cmd
   ;Run cmd was entered
 
-write_cmd:
-  ldx #0
-  lda CMD_LOC,x
-  cmp 'w'
-  bne read_cmd
-  inx
-  lda CMD_LOC,x
-  cmp 'r'
-  bne read_cmd
-  inx
-  lda CMD_LOC,x
-  cmp 'i'
-  bne read_cmd
-  inx
-  lda CMD_LOC,x
-  cmp 't'
-  bne read_cmd
-  inx
-  lda CMD_LOC,x
-  cmp 'e'
-  bne read_cmd
-  ;write cmd was entered
-  jsr exec_write_cmd
-
-read_cmd:
-  ldx #0
-  lda CMD_LOC,x
-  cmp 'r'
-  bne exit_cmd
-  inx
-  lda CMD_LOC,x
-  cmp 'e'
-  bne exit_cmd
-  inx
-  lda CMD_LOC,x
-  cmp 'a'
-  bne exit_cmd
-  inx
-  lda CMD_LOC,x
-  cmp 'd'
-  bne exit_cmd
-  ;read cmd was entered
-  jsr exec_read_cmd
-exit_cmd:
-  rts
 
 ;example program
 ;mimics Input pins to output pins
 exec_run_cmd:
-  lda "x"
-  jsr send_char
-  jsr init_6522 ;setup pin modes
   ;Set data direction registers on 65c22
   lda #%11111111 ; Set all of port B to output
   sta DDRB
@@ -176,8 +110,6 @@ exec_run_cmd:
   sta DDRA
   ;Ensure the outputs begin as off
   sta OIRB
-  lda #0
-  rts
 run_main:
   lda OIRA ; read the inputs into the accumulator
   ;Turn the IO on
@@ -187,58 +119,38 @@ run_main:
 
   ;Repeat
   jmp run_main
+  ;Note: will never return to terminal
 
-exec_write_cmd:
-  ldy #0
-  inx 
-  inx ;skip the space
-  lda CMD_LOC,x
-  cmp #0 ;check if something was written here
-  beq exit_write_cmd
-  sbc #30 ;subtract offset to remove ascii conversion
-  iny ;Keep track of how many places were fetched
-  pha
-  inx
-  lda CMD_LOC,x
-  cmp #0 
-  beq convert_write_cmd
-  sbc #30 ;subtract offset to remove ascii conversion
-  iny
-  pha
-  lda CMD_LOC,x
-  cmp #0
-  beq convert_write_cmd
-  sbc #30 ;subtract offset to remove ascii conversion
-  ;Convert to a single value
-convert_write_cmd:
-  sta WRITE_CMD_LOC
-  ldx WRITE_CMD_LOC ;store first digit in x reg
   
-init_6522:
-  pha
-;Set data direction registers on 65c22
-  lda #%11111111 ; Set all of port B to output
-  sta DDRB
-  lda #%00000000 ; Set port A to inputs
-  sta DDRA
-  ;Ensure the outputs begin as off
-  sta OIRB
-  lda #0
-  pla
-  rts
+;print no cmd error
+inv_cmd:
+    pha
+    ldx #0
+inv_cmd_loop:
+    lda invalid_cmd,x ;Load a with x in string
+    cmp #0 ;If null (end of string)
+    beq inv_cmd_end ;If done with string exit 
+    sta UART_DATA
+    jsr sleep ;Send char over UART_CMD
+    inx
+    jmp inv_cmd_loop
+inv_cmd_end:
+    lda #$a ;newline
+    sta UART_DATA
+    jsr sleep
+    lda #13 ;carriage return
+    sta UART_DATA
+    jsr sleep
+    pla
+    jmp new_line
 
-
-exit_write_cmd:
-  rts
-
-exec_read_cmd:
-  rts
-
-new_cmd_line: .asciiz "User: "
 
 dead_loop:
   jmp dead_loop
 
+
+new_cmd_line: .asciiz "User: "
+invalid_cmd:  .asciiz "Invalid CMD"
 
   .org $fffc ;65c02 fetches at 0xFFFC, store the address of init there to have the CPU set PC to the start of program
   .word init
